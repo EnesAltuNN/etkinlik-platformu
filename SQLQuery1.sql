897a4fa0f78cd023a39cff7d48ca33e327ef2773
Enes Altun <altunenes000@gmail.com>
2025-05-26 02:13:39 +0300

    İlk yükleme - .vs ve sistem dosyaları hariç

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO Users (FullName, Email, PasswordHash, IsApproved, MustChangePassword, IsAdmin)
+VALUES ('Admin Kişi', 'admin@site.com', '123456', 1, 0, 1);
---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO Users (FullName, Email, PasswordHash, IsApproved, MustChangePassword, IsAdmin)
+VALUES ('Admin Kişi', 'admin@site.com',
+'8D969EEF6ECAD3C29A3A629280E686CF0C3F5D5A86AFF3CA12020C923ADC6C92',
+1, 0, 1);
